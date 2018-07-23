e14ea886e3239e412a1f82b0ba7a33f0ca218488
Srinivas1043 <sm1043@gmail.com>
2018-07-23 10:39:06 +0530

    Create consonant_vowel.py

---
+++ consonant_vowel.py
@@ -0,0 +1,9 @@
+
+# your code goes here
+letter = raw_input('Enter any vowel or consonant')
+
+vowel_list = ['a','A','E','I','O','U','e','i','o','u']
+if(letter in vowel_list):
+	print 'Vowel'
+else:
+	print 'Consonant'
